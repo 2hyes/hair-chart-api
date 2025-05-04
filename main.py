f4d358f00461feaba6f6973a7b81cd9dd155496f
2hyes <2hyes.com@gmail.com>
2025-05-04 17:48:15 +0900

    MV main.py to root dir

--- main.py
+++ main.py
@@ -18,11 +18,16 @@ def get_db():
 @app.post("/users/", response_model=schemas.UserRead)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # TODO: password encryption
+    existing_user = db.query(models.User).filter(models.User.id == user.id).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="The user ID is already taken.")
+    
     db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 @app.get("/users/{user_id}", response_model=schemas.UserRead)
 def read_user(user_id: str, db: Session = Depends(get_db)):
