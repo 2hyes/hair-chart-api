61b0486ae6f7ebbbc5a9e94f9c71bd15fc7dfc1c
2hyes <2hyes.com@gmail.com>
2025-05-04 17:48:35 +0900

    ADD db connection retry feature

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,7 +8,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
+MAX_RETRIES = 10
 
-engine = create_engine(DATABASE_URL)
+for attempt in range(MAX_RETRIES):
+    try:
+        engine = create_engine(
+            DATABASE_URL,
+            pool_pre_ping=True, 
+        )
+        with engine.connect() as conn:
+            print("✅ DB Connection successed.")
+        break
+    except Exception as e:
+        print(f"❌ DB Connection failed... Retry:({attempt + 1}/{MAX_RETRIES})")
+        time.sleep(2)
+else:
+    raise RuntimeError("DB Connection failed.")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
