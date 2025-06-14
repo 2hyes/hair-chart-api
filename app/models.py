4301b2c77754effdd2d6266c2c81957ea3473f67
2hyes <2hyes.com@gmail.com>
2025-06-14 22:35:07 +0900

    ADD shop create, read api and FIX user to customer

--- app/models.py
+++ app/models.py
@@ -1,29 +1,28 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP, text, ForeignKey
+from sqlalchemy import Column, Integer, String, TIMESTAMP, text, ForeignKey, Boolean
 from .database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users" # user_type: customer, shop, designer
     
     seq = Column(Integer, primary_key=True, index=True)
     id = Column(String(50), unique=True, nullable=False)
     name = Column(String(100), nullable=False)
     password = Column(String, nullable=False)
-    phone_number = Column(String(20), unique=True)
+    phone_number = Column(String(20), unique=True, nullable=True)
     created_time = Column(TIMESTAMP(timezone=False), server_default=text("CURRENT_TIMESTAMP(0)"), nullable=False)
     updated_time = Column(TIMESTAMP(timezone=False), server_default=text("CURRENT_TIMESTAMP(0)"), nullable=False)
-    memo = Column(String)
+    memo = Column(String, nullable=True)
+    user_type = Column(String(10), nullable=False)
+    is_active = Column(Boolean, nullable=False, server_default=text("true"))
 
-
-class UserHairProfile(Base):
-    __tablename__ = "user_hair_profile"
+class Shop(Base):
+    __tablename__ = "shops"
 
     seq = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String(50), ForeignKey("users.id"), nullable=False)
-    face_shape = Column(String)
-    head_shape = Column(String)
-    personal_color = Column(String)
-    hair_condition = Column(String)
-    scalp_condition = Column(String)
+    id = Column(String(50), ForeignKey("users.id"), nullable=False)
+    name = Column(String(100), nullable=False)
+    number = Column(String(20), nullable=False)
+    biz_number = Column(String(20), nullable=False)
     created_time = Column(TIMESTAMP(timezone=False), server_default=text("CURRENT_TIMESTAMP(0)"), nullable=False)
     updated_time = Column(TIMESTAMP(timezone=False), server_default=text("CURRENT_TIMESTAMP(0)"), nullable=False)
     memo = Column(String)
--- main.py
+++ main.py
@@ -15,48 +15,86 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/users/", response_model=schemas.UserRead)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # TODO: password encryption
-    existing_user = db.query(models.User).filter(models.User.id == user.id).first()
+# TODO: password encryption
+# TODO: refactor whole main.py
+
+@app.post("/customers", response_model=schemas.CustomerRead)
+def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(models.User.id == customer.id).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="The user ID is already taken.")
+        raise HTTPException(status_code=400, detail="User ID is already taken.")
     
-    db_user = models.User(**user.dict())
+    db_user = models.User(**customer.model_dump(), 
+                          user_type="customer")
+
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+@app.post("/shops", response_model=schemas.ShopRead)
+def create_shop(shop: schemas.ShopCreate, db: Session = Depends(get_db)):
+    # Check if user already exists
+    db_user = db.query(models.User).filter(models.User.id == shop.id).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="User ID already taken.")
+    
+    db_user = models.User(
+        id=shop.id,
+        name=shop.name,
+        password=shop.password,
+        phone_number=shop.phone_number,
+        user_type='shop'
+    )
+    db.add(db_user)
+    db.flush()
 
-@app.get("/users/{user_id}", response_model=schemas.UserRead)
-def read_user(user_id: str, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
+    db_shop = models.Shop(
+        id=shop.id,
+        name=shop.name,
+        number=shop.shop_number,
+        biz_number=shop.biz_number
+    )
+    db.add(db_shop)
+    db.commit()
+    db.refresh(db_shop)
+
+    return {
+        "id": db_shop.id,
+        "name": db_shop.name,
+        "phone_number": db_user.phone_number,
+        "shop_number": db_shop.number,
+        "biz_number": db_shop.biz_number,
+        "created_time": db_shop.created_time
+    }
+
+
+@app.get("/customers/{customer_id}", response_model=schemas.CustomerRead)
+def get_customer(customer_id: str, db: Session = Depends(get_db)):
+    user = db.query(models.User).\
+        filter(models.User.id == customer_id).\
+        filter(models.User.user_type == "customer").\
+        first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@app.get("/users/", response_model=List[schemas.UserRead])
-def read_all_users(db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
-
-@app.post("/user-hair-profile/", response_model=schemas.UserHairProfileRead)
-def create_user_hair_profile(profile: schemas.UserHairProfileCreate, db: Session = Depends(get_db)):
-    db_profile = models.UserHairProfile(**profile.dict())
-    db.add(db_profile)
-    db.commit()
-    db.refresh(db_profile)
-    return db_profile
-
-@app.get("/user-hair-profile/{user_id}", response_model=List[schemas.UserHairProfileRead])
-def read_user_hair_profiles(user_id: str, db: Session = Depends(get_db)):
-    profiles = db.query(models.UserHairProfile).filter(models.UserHairProfile.user_id == user_id).order_by(models.UserHairProfile.created_time.desc()).all()
-    return profiles
-
-@app.get("/user-hair-profile/{user_id}/latest", response_model=schemas.UserHairProfileRead)
-def read_latest_user_hair_profile(user_id: str, db: Session = Depends(get_db)):
-    profile = db.query(models.UserHairProfile).filter(models.UserHairProfile.user_id == user_id).order_by(models.UserHairProfile.created_time.desc()).first()
-    if not profile:
-        raise HTTPException(status_code=404, detail="User profile not found")
-    return profile
+@app.get("/shops/{shop_id}", response_model=schemas.ShopRead)
+def get_shop(shop_id: str, db: Session = Depends(get_db)):
+    result = db.query(models.Shop, models.User).\
+        join(models.User, models.Shop.id == models.User.id).\
+        filter(models.Shop.id == shop_id).\
+        first()
+    
+    if result is None:
+        raise HTTPException(status_code=404, detail="Shop not found")
+    
+    shop, user = result
+    return {
+        "id": shop.id,
+        "name": shop.name,
+        "phone_number": user.phone_number,
+        "shop_number": shop.number,
+        "biz_number": shop.biz_number,
+        "created_time": shop.created_time
+    } 
